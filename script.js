8f5f5e8cdbd054d0abc39a5e7841558b11501914
shainasuri <suris546@newschool.edu>
2021-04-30 18:36:27 +0530

    fourth commit

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
 console.log('welcome to the kitchen');
 
-// let inputElement = document.querySelector("#text-input");
-
-// let result = document.querySelector("#text-result");
-
-// let menuResult = document.querySelector("#personal-menu");
-
-// inputElement.addEventListener("keydown",
-// function(event) {
-//     if (event.key == "Enter") {
-//         console.log(inputElement);
-//         result.innerText = "Today you should have: " + inputElement.value;
-        
-
-//         inputElement.value = "";
-//     }
-// }
-// );
-
-
-
-
-// ------------------------------ INNER (FRIDGE.HTML) ------------------------------
+// ------------------------------(FRIDGE.HTML) ------------------------------
 let cuisineResult = document.querySelector('#cuisine');
 let container_1 = document.querySelector('#food-container-1');
 let container_2 = document.querySelector('#food-container-2');
@@ -140,16 +119,4 @@ document.querySelectorAll('.food-containers').forEach(div => {
 
 
 
-
-// ------------------------------ SIDE (CHOICES.HTML) ------------------------------
-
-var superHungry = ['spinach lasagna', 'pizza burrito', 'pizza quesadilla', 'black bean burger', 'cheeseburger', 'fettuccini alfredo', 'enchiladas', 'burrito bowl', 'shepherds pie', 'sloppy joe', 'mashed potatoes', 'loaded nachos', 'spinach lasagna', 'pizza burrito', 'pizza quesadilla', 'black bean burger', 'cheeseburger', 'fettuccini alfredo', 'enchiladas', 'burrito bowl', 'shepherds pie', 'sloppy joe', 'mashed potatoes', 'loaded nachos', 'spaghetti and meatballs', 'pasta', 'penne arrabbiata' ];
-var cheatFood = ['burgers', 'fries', 'pizza', 'pasta', 'burgers', 'fries', 'pizza', 'pasta', 'grilled cheese sandwich', 'burrito', 'tacos', 'nachos', 'noodles', 'fried rice', 'dim sum', 'ice cream', 'chocolate cake', 'cheesecake', 'mac n cheese', 'hot dogs', 'burrito', 'tacos', 'pancakes', 'pizza', 'waffles', 'loaded baked potato', 'chocolate'];
-var midnightSnack = ['bread pizza', 'chili cheese toast', 'egg in a mug', 'nutella on toast', 'PB&J', 'instant noodles', 'ramen', 'potato chips', 'nachos', 'leftover pizza', 'ice cream', 'cheese', 'pasta', 'bananas', 'popcorn', 'cereal', 'butter toast', 'milkshake', 'fruit', 'nuts', 'sandwich', 'leftovers', 'hummus and crackers', 'yogurt', 'oatmeal', 'sweet potato fries', 'pizza', 'nutella on toast'];
-var somethingHealthy = ['bananas', 'nuts', 'apples', 'oranges', 'cucumbers', 'celery', 'quinoa', 'kale', 'avocados', 'watermelon', 'blueberries', 'eggs', 'walnuts', 'carrots', 'green beans', 'yogurt', 'coconut water', 'broccoli', 'mangoes', 'salad', 'smoothie', 'zoodles', 'baked veggie lasagna', 'hummus', 'cauliflower pizza', 'mushroom soup', 'pad thai', 'lentils and rice', 'poke bowl'];
-var snacky = ['chips & dip', 'hummus & pita', 'hummus & veggies', 'pesto bruschetta', 'chocolate covered blueberries', 'popcorn', 'pretzels', 'mixed nuts', 'guacamole', 'nachos', 'almonds', 'dark chocolate', 'apples', 'oranges', 'granola bar', 'sweet potato chips', 'potato chips', 'tater tots', 'jalapeño poppers', 'tomato bruschetta', 'cookies', 'oreos', 'croissants', 'mixed berries', 'gummy bears', 'sandwich', 'candy'];
-var bored = ['nutella on toast', 'PB&J', 'fries', 'banana bread', 'homemade donuts', 'mac n cheese', 'matcha oreos', 'oreos', 'funfetti cake', 'giant cinammon roll', 'tater tots', 'ramen', 'tacos', 'soda floats', 'donut milkshake', 'cookies and cream shake', 'avocado toast', 'spaghetti', 'pizza quesadilla', 'jalapeño poppers', 'smores', 'ice cream', 'cereal', 'mermaid toast', 'bagel', 'sushi', 'pancakes'];
-var tired = ['bananas', 'coffee', 'sweet potato fries', 'eggs', 'apples', 'dark chocolate', 'goji berries', 'quinoa', 'oatmeal', 'yogurt', 'hummus', 'edamame', 'lentils', 'avocados', 'oranges', 'strawberries', 'seeds', 'beans', 'green tea', 'nuts', 'popcorn', 'beets', 'almonds', 'spinach', 'watermelon', 'dates']; 
-var unwell = ['soup', 'broth', 'garlic', 'coconut water', 'peppermint tea', 'honey', 'ginger', 'spicy food', 'bananas', 'oatmeal', 'yogurt', 'avocados', 'leafy green vegetables', 'chamomile tea', 'licorice', 'flaxseed', 'papaya', 'buttermilk', 'kombucha', 'toast', 'porridge', 'rice', 'fruit juice', 'carrots', 'applesauce', 'sweet potatoes', 'eggs'];
-var dessert = ['ice cream', 'apple pie', 'tiramisu', 'brownie', 'cookies', 'cake', 'waffles', 'jalebi', 'churros', 'cheesecake', 'custard', 'cinnamon rolls', 'eclairs', 'gelato', 'french toast', 'flan', 'boba', 'lemon cake', 'chocolate cake', 'banana bread', 'popsicle', 'scones', 'smores', 'pudding', 'marshmallows', 'chocolate', 'jellybeans'];
 
---
+++ script2.js
@@ -0,0 +1,72 @@
+// ------------------------------ (CHOICES.HTML) ------------------------------
+
+// var superHungry = ['spinach lasagna', 'pizza burrito', 'pizza quesadilla', 'black bean burger', 'cheeseburger', 'fettuccini alfredo', 'enchiladas', 'burrito bowl', 'shepherds pie', 'sloppy joe', 'mashed potatoes', 'loaded nachos', 'spinach lasagna', 'pizza burrito', 'pizza quesadilla', 'black bean burger', 'cheeseburger', 'fettuccini alfredo', 'enchiladas', 'burrito bowl', 'shepherds pie', 'sloppy joe', 'mashed potatoes', 'loaded nachos', 'spaghetti and meatballs', 'pasta', 'penne arrabbiata' ];
+// var cheatFood = ['burgers', 'fries', 'pizza', 'pasta', 'burgers', 'fries', 'pizza', 'pasta', 'grilled cheese sandwich', 'burrito', 'tacos', 'nachos', 'noodles', 'fried rice', 'dim sum', 'ice cream', 'chocolate cake', 'cheesecake', 'mac n cheese', 'hot dogs', 'burrito', 'tacos', 'pancakes', 'pizza', 'waffles', 'loaded baked potato', 'chocolate'];
+// var midnightSnack = ['bread pizza', 'chili cheese toast', 'egg in a mug', 'nutella on toast', 'PB&J', 'instant noodles', 'ramen', 'potato chips', 'nachos', 'leftover pizza', 'ice cream', 'cheese', 'pasta', 'bananas', 'popcorn', 'cereal', 'butter toast', 'milkshake', 'fruit', 'nuts', 'sandwich', 'leftovers', 'hummus and crackers', 'yogurt', 'oatmeal', 'sweet potato fries', 'pizza', 'nutella on toast'];
+// var somethingHealthy = ['bananas', 'nuts', 'apples', 'oranges', 'cucumbers', 'celery', 'quinoa', 'kale', 'avocados', 'watermelon', 'blueberries', 'eggs', 'walnuts', 'carrots', 'green beans', 'yogurt', 'coconut water', 'broccoli', 'mangoes', 'salad', 'smoothie', 'zoodles', 'baked veggie lasagna', 'hummus', 'cauliflower pizza', 'mushroom soup', 'pad thai', 'lentils and rice', 'poke bowl'];
+// var snacky = ['chips & dip', 'hummus & pita', 'hummus & veggies', 'pesto bruschetta', 'chocolate covered blueberries', 'popcorn', 'pretzels', 'mixed nuts', 'guacamole', 'nachos', 'almonds', 'dark chocolate', 'apples', 'oranges', 'granola bar', 'sweet potato chips', 'potato chips', 'tater tots', 'jalapeño poppers', 'tomato bruschetta', 'cookies', 'oreos', 'croissants', 'mixed berries', 'gummy bears', 'sandwich', 'candy'];
+// var bored = ['nutella on toast', 'PB&J', 'fries', 'banana bread', 'homemade donuts', 'mac n cheese', 'matcha oreos', 'oreos', 'funfetti cake', 'giant cinammon roll', 'tater tots', 'ramen', 'tacos', 'soda floats', 'donut milkshake', 'cookies and cream shake', 'avocado toast', 'spaghetti', 'pizza quesadilla', 'jalapeño poppers', 'smores', 'ice cream', 'cereal', 'mermaid toast', 'bagel', 'sushi', 'pancakes'];
+// var tired = ['bananas', 'coffee', 'sweet potato fries', 'eggs', 'apples', 'dark chocolate', 'goji berries', 'quinoa', 'oatmeal', 'yogurt', 'hummus', 'edamame', 'lentils', 'avocados', 'oranges', 'strawberries', 'seeds', 'beans', 'green tea', 'nuts', 'popcorn', 'beets', 'almonds', 'spinach', 'watermelon', 'dates']; 
+// var unwell = ['soup', 'broth', 'garlic', 'coconut water', 'peppermint tea', 'honey', 'ginger', 'spicy food', 'bananas', 'oatmeal', 'yogurt', 'avocados', 'leafy green vegetables', 'chamomile tea', 'licorice', 'flaxseed', 'papaya', 'buttermilk', 'kombucha', 'toast', 'porridge', 'rice', 'fruit juice', 'carrots', 'applesauce', 'sweet potatoes', 'eggs'];
+// var dessert = ['ice cream', 'apple pie', 'tiramisu', 'brownie', 'cookies', 'cake', 'waffles', 'jalebi', 'churros', 'cheesecake', 'custard', 'cinnamon rolls', 'eclairs', 'gelato', 'french toast', 'flan', 'boba', 'lemon cake', 'chocolate cake', 'banana bread', 'popsicle', 'scones', 'smores', 'pudding', 'marshmallows', 'chocolate', 'jellybeans'];
+
+
+var moods = new Array ('super hungry', 'cheat food', 'midnight snack', 'something healthy', 'snacky', 'tired', 'bored', 'unwell', 'dessert');
+
+// super hungry
+moods[0] = new Array ('spinach lasagna', 'pizza burrito', 'pizza quesadilla', 'black bean burger', 'cheeseburger', 'fettuccini alfredo', 'enchiladas', 'burrito bowl', 'shepherds pie', 'sloppy joe', 'mashed potatoes', 'loaded nachos', 'spinach lasagna', 'pizza burrito', 'pizza quesadilla', 'black bean burger', 'cheeseburger', 'fettuccini alfredo', 'enchiladas', 'burrito bowl', 'shepherds pie', 'sloppy joe', 'mashed potatoes', 'loaded nachos', 'spaghetti and meatballs', 'pasta', 'penne arrabbiata' );
+// cheat food
+moods[1] = new Array ('burgers', 'fries', 'pizza', 'pasta', 'burgers', 'fries', 'pizza', 'pasta', 'grilled cheese sandwich', 'burrito', 'tacos', 'nachos', 'noodles', 'fried rice', 'dim sum', 'ice cream', 'chocolate cake', 'cheesecake', 'mac n cheese', 'hot dogs', 'burrito', 'tacos', 'pancakes', 'pizza', 'waffles', 'loaded baked potato', 'chocolate');
+// midnight snack
+moods[2] = new Array ('bread pizza', 'chili cheese toast', 'egg in a mug', 'nutella on toast', 'PB&J', 'instant noodles', 'ramen', 'potato chips', 'nachos', 'leftover pizza', 'ice cream', 'cheese', 'pasta', 'bananas', 'popcorn', 'cereal', 'butter toast', 'milkshake', 'fruit', 'nuts', 'sandwich', 'leftovers', 'hummus and crackers', 'yogurt', 'oatmeal', 'sweet potato fries', 'pizza', 'nutella on toast');
+// something healthy
+moods[3] = new Array ('bananas', 'nuts', 'apples', 'oranges', 'cucumbers', 'celery', 'quinoa', 'kale', 'avocados', 'watermelon', 'blueberries', 'eggs', 'walnuts', 'carrots', 'green beans', 'yogurt', 'coconut water', 'broccoli', 'mangoes', 'salad', 'smoothie', 'zoodles', 'baked veggie lasagna', 'hummus', 'cauliflower pizza', 'mushroom soup', 'pad thai', 'lentils and rice', 'poke bowl');
+// snacky
+moods[4] = new Array ('chips & dip', 'hummus & pita', 'hummus & veggies', 'pesto bruschetta', 'chocolate covered blueberries', 'popcorn', 'pretzels', 'mixed nuts', 'guacamole', 'nachos', 'almonds', 'dark chocolate', 'apples', 'oranges', 'granola bar', 'sweet potato chips', 'potato chips', 'tater tots', 'jalapeño poppers', 'tomato bruschetta', 'cookies', 'oreos', 'croissants', 'mixed berries', 'gummy bears', 'sandwich', 'candy');
+// tired
+moods[5] = new Array ('bananas', 'coffee', 'sweet potato fries', 'eggs', 'apples', 'dark chocolate', 'goji berries', 'quinoa', 'oatmeal', 'yogurt', 'hummus', 'edamame', 'lentils', 'avocados', 'oranges', 'strawberries', 'seeds', 'beans', 'green tea', 'nuts', 'popcorn', 'beets', 'almonds', 'spinach', 'watermelon', 'dates');
+// bored
+moods[6] = new Array ('nutella on toast', 'PB&J', 'fries', 'banana bread', 'homemade donuts', 'mac n cheese', 'matcha oreos', 'oreos', 'funfetti cake', 'giant cinammon roll', 'tater tots', 'ramen', 'tacos', 'soda floats', 'donut milkshake', 'cookies and cream shake', 'avocado toast', 'spaghetti', 'pizza quesadilla', 'jalapeño poppers', 'smores', 'ice cream', 'cereal', 'mermaid toast', 'bagel', 'sushi', 'pancakes');
+// unwell
+moods[7] = new Array ('soup', 'broth', 'garlic', 'coconut water', 'peppermint tea', 'honey', 'ginger', 'spicy food', 'bananas', 'oatmeal', 'yogurt', 'avocados', 'leafy green vegetables', 'chamomile tea', 'licorice', 'flaxseeds', 'papaya', 'buttermilk', 'kombucha', 'toast', 'porridge', 'rice', 'fruit juice', 'carrots', 'applesauce', 'sweet potatoes', 'soup');
+// dessert
+moods[8] = new Array ('ice cream', 'apple pie', 'tiramisu', 'brownie', 'cookies', 'cake', 'waffles', 'jalebi', 'churros', 'cheesecake', 'custard', 'cinnamon rolls', 'eclairs', 'gelato', 'french toast', 'flan', 'boba', 'lemon cake', 'chocolate cake', 'banana bread', 'popsicle', 'scones', 'smores', 'pudding', 'marshmallows', 'chocolate', 'jellybeans');
+
+console.log();
+
+// function showMoods () {
+//     for (i = 0; i < moods.length; i++)
+//     {
+//       var moods = document.createElement("li");
+//       var moodCategory = document.createElement("p");
+//       moods.append(moodCategory);
+//       document.querySelector("#moods").append(moods);
+//     }
+// }
+
+for (i = 0; i < moods.length; i++) {
+    showMoods(moods[i]);
+}
+
+
+function showMoods(moods) {
+    var moods = document.createElement("li");
+      var moodCategory = document.createElement("p");
+      moods.append(moodCategory);
+      document.querySelector("#moods").append(moods);
+}
+
+
+
+let inputElement = document.querySelector("#name-input");
+let result = document.querySelector("#name-result");
+
+let menuResult = document.querySelector("#personal-menu");
+
+inputElement.addEventListener("keydown", function(event) {
+    if (event.key == "Enter") {
+        console.log(inputElement);
+        result.innerText = "Hey," + inputElement.value;
+        inputElement.value = "";
+    }
+}); 
