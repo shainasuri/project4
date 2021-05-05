910afbd9fe0745c5f7be2f4837acaae9cf7be7f7
shainasuri <suris546@newschool.edu>
2021-05-05 21:58:47 +0530

    fifth commit

--- script2.js
+++ script2.js
@@ -1,18 +1,6 @@
 // ------------------------------ (CHOICES.HTML) ------------------------------
 
 var moods = new Array ('super hungry', 'snacky', 'cheat food', 'tired', 'something healthy', 'bored', 'unwell', 'midnight snack', 'dessert');
-function showMoods () {
-    for (i = 0; i < moods.length; i++)
-    {
-        var categories = document.createElement("li");
-        var moodCategory = document.createElement("p");
-        moodCategory.innerHTML = moods[i];
-        categories.classList.add("mood-magnet")
-        categories.append(moodCategory);
-        document.querySelector("#moods").append(categories);
-    }
-}
-showMoods();
 
 var recommendations = []
 // super hungry
@@ -34,34 +22,78 @@ recommendations[7] = new Array ('soup', 'broth', 'garlic', 'coconut water', 'pep
 // dessert
 recommendations[8] = new Array ('ice cream', 'apple pie', 'tiramisu', 'brownie', 'cookies', 'cake', 'waffles', 'jalebi', 'churros', 'cheesecake', 'custard', 'cinnamon rolls', 'eclairs', 'gelato', 'french toast', 'flan', 'boba', 'lemon cake', 'chocolate cake', 'banana bread', 'popsicle', 'scones', 'smores', 'pudding', 'marshmallows', 'chocolate', 'jellybeans');
 
-console.log(recommendations[0]);
+console.log(recommendations[0][Math.floor(Math.random() * recommendations[0].length)]);
 
 
-
+// function showMoods () {
+//     for (i = 0; i < moods.length; i++)
+//     {
+//         var categories = document.createElement("li");
+//         var moodCategory = document.createElement("p");
+//         moodCategory.innerHTML = moods[i];
+//         categories.classList.add("mood-magnet")
+//         categories.append(moodCategory);
+//         document.querySelector("#moods").append(categories);
+//     }
+// }
+// showMoods();
 
 let inputElement = document.querySelector("#name-input");
 let result = document.querySelector("#name-result");
 
+inputElement.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        document.querySelector("#final-result").append(result);
+        result.innerText = "Hey, " + inputElement.value + "!";
+        inputElement.value = "";
+        document.getElementById("super-hungry").classList.remove("hidden");
+        document.getElementById("snacky").classList.remove("hidden");
+        document.getElementById("cheat-food").classList.remove("hidden");
+        document.getElementById("tired").classList.remove("hidden");
+        document.getElementById("something-healthy").classList.remove("hidden");
+        document.getElementById("bored").classList.remove("hidden");
+        document.getElementById("unwell").classList.remove("hidden");
+        document.getElementById("midnight-snack").classList.remove("hidden");
+        document.getElementById("dessert").classList.remove("hidden");
+        document.getElementById("prompt-2").classList.remove("hidden");
+        document.getElementById("name-prompt").classList.add("hidden");
+    }
+}); 
 
-let menuResult = document.querySelector("#personal-menu");
-
-
-
-
-
+ 
 $(document).ready(function(){
     $(".mood-magnet").click(function(){
         $(this).css("box-shadow", "inset 0px 2px 4px rgba(0, 0, 0, 0.25)");
         $(this).siblings(".mood-magnet").css("box-shadow", "none");
-        $("#name-prompt").removeClass("enter-name");
     });
 });
+ 
+// const embedElements = () => {
+//     recommendations[0].forEach(element => {
+//        document.getElementById('superhungry').innerHTML +=
+//        `<p>${element}</p>`;
+//     });
+// };
+ 
+ 
+let superHungry = document.querySelector("#super-hungry");
+let finalResult =  document.querySelector("#food-result")
 
-inputElement.addEventListener("keydown", function(event) {
-    if (event.key == "Enter") {
-        console.log(inputElement);
-        document.querySelector("#final-result").append(result);
-        result.innerText = "Hey, " + inputElement.value + "!";
-        inputElement.value = "";
-    }
-}); 
+
+superHungry.addEventListener("click", function(event){
+    document.querySelector("#final-result").append(finalResult);
+    finalResult.innerText = "Have some " + recommendations[0][Math.floor(Math.random() * recommendations[0].length)] + " today!";
+   
+});
+
+
+ 
+// var categories = document.createElement("li");
+//         var moodCategory = document.createElement("p");
+//         moodCategory.innerHTML = moods[i];
+//         categories.classList.add("mood-magnet")
+//         categories.append(moodCategory);
+//         document.querySelector("#moods").append(categories);
+
+
+console.log(recommendations[0][Math.floor(Math.random() * recommendations[0].length)]);
